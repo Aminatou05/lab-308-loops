6de395ec5844a02938ae7c3d76315444c7647470
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-02-26 18:49:27 -0800

    final commit

--- lab.js
+++ lab.js
@@ -53,8 +53,17 @@ let num = 4;
 //  You may be familiar with CSVs through past use of programs such as Microsoft Excel or Google Sheets. 
 //  While each of these programs save their data in different formats to preserve style (e.g., 
 //     font color or cell backgrounds), at their core, they are storing CSV data.
-
+//part 3
 let data = "ID, Name, Occupation, Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
 let rows = data.split("\n");
 
 console.log(rows);
+
+const csvString = "ID, Name, Occupation, Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+for(const char of csvString ) {
+    console.log(char);
+}
+
+let index = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+let rows1 = index.split("\n");
+console.log(rows1);
